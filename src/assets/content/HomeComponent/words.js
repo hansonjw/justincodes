1a8377ddb693681b680def26bfbbdd5f907b44d2
hansonjw <hansonjw@gmail.com>
2023-01-03 15:09:01 -0600

    Updated University wording/content

--- src/assets/content/HomeComponent/words.js
+++ src/assets/content/HomeComponent/words.js
@@ -1,53 +1,52 @@
 const WORDS = {
     uco: [
         "I have an analytical mindset and a strong interest in math.  This led me to the University of Colorado where I studied Mechanical Engineering as an undergraduate.",
-        "Much of my time at CU was spent in the UNIX workstation lab where I used cutting edge software to build 3D models (Computer Aided Drafting - CAD), compute stress and strain of mechanical systems (Finite Element Analysis - FEA), calculate mathematical solutions with Matlab, and model and temperature gradients and fluid flow (Computation Fluid Dynamics - CFD).",
+        "Much of my time at CU was spent in the UNIX workstation lab where I used cutting edge software to build 3D models (Computer Aided Drafting - CAD), compute stress and strain of mechanical systems (Finite Element Analysis - FEA), calculate advanced mathematics with Matlab, and model temperature gradients and fluid flow (Computation Fluid Dynamics - CFD).",
         "One of my favorite courses at CU was a Computer Science course which taught us the fundamentals of Data Structures and Algorithms through programming in C++.",
-        "It was impossible to see my future career at the time, however, my time spent in the computer lab laid a strong foundation in analysis, data, computation, and hardware.  My focus shifted from mechanical systems to utilizing software tools to solve problems."
+        "It was impossible to see my future career at the time, however, my time spent in the computer lab laid a strong foundation in analysis, data, and computation.  Later, as my career developed, my focus shifted from mechanical systems to utilizing software to solve all kinds of problems."
     ],
     
     und: [
-        "Early in my career I received my first exposure to business and quickly saw that financial acumen was crucial to good decision making.  My interest in economics as an academic subject was piqued and began to sense the power in sound investment analysis.",
-        "At this time the world began to change in a big way, driven by technology.  The financial industry was entering the era of ‘big data’ and was calling for analytical minds and people with strong computing skills.  Getting an MBA was a logical next step for me and joined two academic interests of mine together - technology and economics.",
+        "Early in my career I received my first exposure to business and quickly saw that financial acumen was crucial to good decision making.  My interest in economics was piqued.  I began to sense the liberating advantages in disciplined investment analysis.",
+        "At this time the world began to change in a big way.  The financial industry was entering the era of ‘big data’ and was calling for analytical and computing skills.  Given my technical background and interest in economics, getting an MBA was a logical next step for me.",
         "At Notre Dame I found a wonderful community of disciplined, intellectually curious thinkers and learners.  At this great institution I was exposed to great financial minds like Warren Buffett and became fascinated by investing and building economic prosperity."
         ],
     
     em: [],
     
     uca: [
-        "The world has changed so fast over the last ~20 years.  I like to think back to the early 1900’s when humanity flew for the first time, illuminated the world with electricity, developed the automobile and the assembly line, made huge advances in medicine, and began to communicate across the world in real time through radio and television.  These technological advancements led to “the roaring 20’s”, a period of rapid economic growth. The parallels of “the roaring ’20’s” with the technological and economic advancements today are fascinating to contemplate and hard to ignore.  This new technology economy is calling for people with a diverse skill set and broad knowledge base. I want to be one of those people on the cutting edge.",
-        "With a clear sense of my strengths and interests, I sought a formal environment to learn code.",
+        "I like to think back to the early 20th century when humanity flew for the first time, illuminated the world with electricity, invented the automobile, developed the assembly line, made huge advances in medicine, and began to communicate across the world in real time through radio and television.  These technological advancements led to a period called “the roaring 20’s”, a period of rapid economic growth prosperity. The parallels of “the roaring ’20’s” with today are hard to ignore.",
+        "I want to be one of those people on the cutting edge of this new technology driven economy.  To that end and with a clear sense of my strengths and interests, I sought a formal and rigorous environment to learn code.",
         "I found a perfect fit with the full-stack web-development course at the University of California Berkeley Extension.  In this course I developed a strong foundation in JavaScript, web technologies, and computer science fundamentals."
     ],
     
     intro: [
-        "I am a full stack developer with a strong curiosity in the impact of math, science, economics, and technology to humanity.",
-        "My experience is well-rounded and includes working for ExxonMobil - one of the world's oldest and most successful bluechip companies.",
+        "I am a full stack developer with a strong curiosity in math, science, economics, and technology.  I'm facinated by how these subjects are impacting humanity.",
+        "My experience is well-rounded and includes working for ExxonMobil - one of the world's oldest and most successful bluechip corporations.",
         "I also have a strong academic background with degrees from the University of Colorado, and the University of Notre Dame - two wonderful academic institutions.",
-        "Please have a look around!"
     ],
     exxonmobil: [
         `The scale of the energy industry is unparalleled in business and ExxonMobil is a world-class leader.  I am grateful to have worked for this great company.`,
         `At ExxonMobil I leveraged big data, code, technology, and data science to drive sound economic decisions.  This is now a passion.`,
-        `Please have a look through the slides to see where I worked!`
+        `Please have a look through the slides to see my work experience.`
     ],
     beaumont: [
-        `In ExxonMobil’s world class employee development program I saw first hand the operational magnitude required to convert oil into economically useful products for humanity.`,
-        `I worked at the 5th largest oil refinery in the United States located in Beaumont, TX.  I served as the ‘right-hand-man’ to the Executive leadership team, analyzing data to support leadership decisions and day-to-day operations.`
+        `In ExxonMobil’s world class employee development program I saw first hand the operational magnitude required to convert oil into economically viable products for humanity.`,
+        `I worked at the 5th largest oil refinery in the United States located in Beaumont, TX.  There I served as the ‘right-hand-man’ to the Executive leadership team, analyzing data and generating useful information to support leadership decisions.`
     ],
     midland: [
-        `This is West Texas, the epicenter of a major oil production renaissance beginning in the mid 2000s.  The US became the world’s largest oil producer by 2018 surpassing Russia and Saudi Arabia.`,
+        `This is West Texas, the epicenter of a major oil production renaissance beginning in the mid 2000s.  During this renaissance the US began to double it's oil production capacity and, by 2018, became the world’s largest oil producer surpassing Russia and Saudi Arabia.`,
         `I took this picture while flying into the Midland, TX airport.  You can see the oil fields stretching out to the horizon in all directions.`
     ],
     permian: [
-        `The amount of steel, water, raw materials, and energy needed to extract oil from the ground boggles my mind.  In the picture you can see tank batteries, pumps, and power lines stretching out for hundreds of miles into the West Texas desert.`
+        `The amount of steel, water, raw materials, and energy needed to extract oil from the ground boggles my mind to this day.  In the picture you can see tank batteries, pumps, and power lines stretching out for hundreds of miles into the West Texas desert.`
     ],
     champion: [
-        `I built dashboards to monitor daily activity of ExxonMobil's fleet of drill-ships deployed all around the globe.`,
-        `Pictured here is the Deepwater Champion which has sailed to the Gulf of Mexico, the Black Sea, off the coast of Africa, and more!`
+        `I built dashboards to help Executives monitor daily activity of ExxonMobil's fleet of drill-ships deployed all around the globe.`,
+        `Pictured here is the Deepwater Champion which has sailed to the Gulf of Mexico, the Black Sea, off the coast of Africa, and more.`
     ],
     acquarius: [
-        `Contracts on these large drill-ships cost upwards of half-a-million dollars per day or more, and are often times contracted for multiple years at a time.  Getting the negotiations right and understanding the markets for these drill-ships could result in ten’s-of-millions of dollars in savings for ExxonMobil.`
+        `Contracts on these large drill-ships cost upwards of half-a-million dollars per day and more, and are often times contracted for multiple years at a time.  Getting the negotiations right and leveraging data for market intelligence could result in ten’s-of-millions of dollars in savings for ExxonMobil.`
     ],
 }
 
