0170c6d246d00733323f5d81d9bd80141b52ae02
hansonjw <hansonjw@gmail.com>
2025-07-29 07:01:49 -0700

    added message for book hotel by date

--- src/pages/Wedding/assets/words.js
+++ src/pages/Wedding/assets/words.js
@@ -18,7 +18,7 @@ const CONTENT = [
     },
     {
         title: "Hotel",
-        textlist: ["Marriott Golden, CO", "Regina Jackman and Justin Hanson Wedding Block", "Start Date: Thursday, September 25, 2025", "End Date: Sunday, September 28, 2025"],
+        textlist: ["Marriott Golden, CO", "Regina Jackman and Justin Hanson Wedding Block", "Start Date: Thursday, September 25, 2025", "End Date: Sunday, September 28, 2025", "Please book your room by August 28, 2025"],
         // image: require('./images/ja.jpeg'),
         link: "https://www.marriott.com/event-reservations/reservation-link.mi?id=1748439076854&key=GRP&guestreslink2=true&app=resvlink",
         map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9430.248944070989!2d-105.170355807623!3d39.73976942851277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b845ca799f307%3A0xddf961a2492b636f!2sDenver%20Marriott%20West!5e0!3m2!1sen!2sus!4v1749430517991!5m2!1sen!2sus",
